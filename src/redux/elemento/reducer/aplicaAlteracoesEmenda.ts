f582b09cac562d37a63720483474c6baa2584982
João Matos <joaofmatos.10@gmail.com>
2026-07-06 16:44:03 -0300

    fix: corrige tipoEmenda de agrupador ao reabrir emenda "Artigo Onde Couber"

--- src/redux/elemento/reducer/aplicaAlteracoesEmenda.ts
+++ src/redux/elemento/reducer/aplicaAlteracoesEmenda.ts
@@ -185,7 +185,7 @@ const criaEventosParaDispositivoAgrupador = (state: any, dea: DispositivoEmendaA
 
     const novo = getDispositivoFromElemento(articulacao, novoAgrupador)!;
 
-    ajustaAtributosDispositivoAdicionado(novo, dea, ClassificacaoDocumento.EMENDA);
+    ajustaAtributosDispositivoAdicionado(novo, dea, state.modo);
 
     elementosIncluidos.length = 0;
     elementosIncluidos.push(createElemento(novo));
